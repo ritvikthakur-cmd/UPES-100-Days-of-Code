32b8dc5043fe8c7675866b861235dd86e25aa0c8
Ritvik Singh Thakur <ritvik.thakur0810@gmail.com>
2026-08-15 19:36:44 +0530

    Add files via upload

---
+++ Day6(ii).c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    if (num >= 0) {
+        if (num == 0) {
+            printf("%d is zero\n", num);
+        } else {
+            printf("%d is positive\n", num);
+        }
+    } else {
+        printf("%d is negative\n", num);
+    }
+    return 0;
+}
